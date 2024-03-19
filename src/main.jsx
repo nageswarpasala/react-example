cb312a2fc2e436144c0fd1f4c69afe406637bba4
nageswarpasala <nageswarreddy12@gmail.com>
2024-03-18 20:03:49 -0500

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const myFirstElement = <h1>My first element</h1>;
+const myFirstElement = <h1>My first simple react application</h1>;
 
 ReactDOM.createRoot(document.getElementById("root")).render(myFirstElement);
